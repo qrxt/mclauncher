dff9c663a9b26c44a5925b5a926ac699065a7c8c
qrxt <qrx357@gmail.com>
2022-09-07 02:21:36 +0300

    Add CI
    Add Makefile
    Add eslint & plugins
    Add prettier & plugins
    Add jest

---
+++ Makefile
@@ -0,0 +1,49 @@
+# Dev
+dev:
+	npm run tauri dev
+
+kill:
+	sudo kill -9 `sudo lsof -t -i:1420`
+
+# Tests
+lint-rs:
+	cd ./src-tauri/ && cargo fmt --check
+	cd ./src-tauri/ && cargo clippy
+
+test-rs:
+	cd ./src-tauri/ && cargo test
+
+check-rust-all:
+	make lint-rs
+	make test-rs
+
+fmt-js:
+	npx -p prettier@latest -p pretty-quick pretty-quick
+
+test-js:
+	npm test
+
+lint-js:
+	npm run lint
+
+check-js-all:
+	make fmt-js
+	make lint-js
+	make test-js
+
+lint:
+	make lint-rs
+	make lint-js
+
+test:
+	make check-rust-all
+	make check-js-all
+
+# Build
+
+# ...
+
+# Setup
+setup:
+	npm i
+	cd ./src-tauri && cargo build
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import React from "react";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -16,13 +17,13 @@ function App() {
       <h1>Welcome to Tauri!</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-        <a href="https://tauri.app" target="_blank">
+        <a href="https://tauri.app">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
