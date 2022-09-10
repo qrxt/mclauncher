c120fb58b025ebbcaf5d41623a664cc778617032
qrxt <qrx357@gmail.com>
2022-09-10 20:05:32 +0300

    Add instances and settings pages

---
+++ src/components/pages/InstancesPage/InstancesPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Page from "components/Page";
+
+function InstancesPage() {
+  return (
+    <Page>
+      <p>Instances Page</p>
+    </Page>
+  );
+}
+
+export default InstancesPage;
